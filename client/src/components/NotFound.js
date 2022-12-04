37a10afb90407deb6935a27c40acdc80d2a631a5
srpss <martinslavovforfut1@gmail.com>
2022-12-04 11:40:10 +0200

    remove old client

--- client/src/components/NotFound.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-export default function NotFound() {
-  return (
-    <div className='not-found'>You need to login first <Link to="/login">Login</Link></div>
-  )
-}
