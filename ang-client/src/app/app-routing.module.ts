6c131d79ad8fb4d00852a6e59f2fcccaf0edf2ad
srpss <61586478+srpss@users.noreply.github.com>
2022-12-06 05:25:02 +0200

    client - routing to components

--- ang-client/src/app/app-routing.module.ts
+++ ang-client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MainComponent } from './main/main.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{path: 'register', component: RegisterComponent},
+{path: 'login', component: LoginComponent},
+{path: 'profile', component: ProfileComponent},
+{path: '', component: MainComponent},
+{path: 'SearchComponent', component: SearchComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
